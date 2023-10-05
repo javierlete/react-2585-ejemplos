341fd2b3e533e3e219b1bb18c05f98bce98e00b0
Javier Lete <javier@eskura21.com>
2023-10-05 19:36:57 +0200

    Evento de cambio para LabelInput

--- src/componentes/LabelInput.js
+++ src/componentes/LabelInput.js
@@ -1,10 +1,10 @@
-export default function LabelInput({ etiqueta, id, nombre, tipo, valor }) {
+export default function LabelInput({ etiqueta, id, nombre, tipo, valor, onCambio }) {
     nombre = nombre || id;
 
     return (
         <div>
             <label htmlFor={id}>{etiqueta}</label>
-            <input id={id} name={nombre} type={tipo} defaultValue={valor} />
+            <input id={id} name={nombre} type={tipo} defaultValue={valor} onChange={e => onCambio(e.target.value)} />
         </div>
     );
 }
