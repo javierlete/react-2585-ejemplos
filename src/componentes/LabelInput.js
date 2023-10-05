3acf7186492e050d57bd66d00eb0d700570480b2
Javier Lete <javier@eskura21.com>
2023-10-05 20:04:19 +0200

    Añadido Bootstrap

--- src/componentes/LabelInput.js
+++ src/componentes/LabelInput.js
@@ -2,9 +2,13 @@ export default function LabelInput({ etiqueta, id, nombre, tipo, valor, onCambio
     nombre = nombre || id;
 
     return (
-        <div>
-            <label htmlFor={id}>{etiqueta}</label>
-            <input id={id} name={nombre} type={tipo} defaultValue={valor} onChange={e => onCambio(e.target.value)} />
+        <div className="row mb-3">
+            <label className="col-sm-2 col-form-label" htmlFor={id}>{etiqueta}</label>
+            <div class="col-sm">
+                <input className={tipo === 'submit' ? 'btn btn-primary' : 'form-control'}
+                    value={valor} id={id} name={nombre} type={tipo} 
+                    defaultValue={valor} onChange={e => onCambio(e.target.value)} />
+            </div>
         </div>
     );
 }
